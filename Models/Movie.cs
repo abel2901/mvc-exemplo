31e334441f3577ffb0ba158ee352ae23ba29528a
Abel Silva <abel2901@gmail.com>
2022-03-18 11:03:05 -0300

    Melhorar aparência das datas

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace ContosoUniversity.Models
@@ -10,6 +8,9 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
